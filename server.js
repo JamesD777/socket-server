4276c6a1bd6048759a1e0b81830fcf727ac00810
JamesD <jcd4912@gmail.com>
2018-02-25 06:58:34 -0600

    arduino start

--- server.js
+++ server.js
@@ -3,13 +3,23 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
-    console.log('sent: %s', message);
-  });
+	ws.on('message', function incoming(message) {
+		if (message === 'arduino') {
+	  	// Broadcast to unity that the arduino joined
+	  	wss.clients.forEach(function each(client) {
+	  		if (client !== ws && client.readyState === WebSocket.OPEN) {
+	  			client.send(message);
+	  		}
+	  	});
+	  } else {
+	  	console.log('received: %s', message);
+	  	wss.clients.forEach(function each(client) {
+	  		if (client !== ws && client.readyState === WebSocket.OPEN) {
+	  			client.send(message);
+	  		}
+	  	});
+	  	console.log('sent: %s', message);
+	  }
+	  
+	});
 });
