edc00ae9fb9f0a944781e6e18f5787360057dfa4
JamesD <jcd4912@gmail.com>
2018-02-25 05:56:06 -0600

    working with arduino

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const wss = new WebSocket.Server({ port: 8080 });
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
-    ws.send(message);
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
     console.log('sent: %s', message);
   });
 });
